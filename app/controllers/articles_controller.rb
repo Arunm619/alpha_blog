37396cf76c72b1f7f20c621123a513df023a2794
Arun sudharsan <arun.m.sudharsan@gmail.com>
2019-01-16 14:15:07 +0530

    added articles listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,18 @@ def new
 	@article = Article.new
 end
 
+
+def edit
+@article = Article.find(params[:id])
+end
+
 def show 
 @article = Article.find(params[:id])
+end
+
+
+def index
+	@articles = Article.all
 end
 
 def create
@@ -18,8 +28,16 @@ def create
 else
 	render 'new'
 	end
+end
+def update
+	 @article = Article.find(params[:id])
 
-
+	if @article.update(article_params)
+		flash[:notice] = "article was successfully updated"
+		redirect_to article_path(@article)
+	else
+		render 'edit'
+	end
 
 end
 
