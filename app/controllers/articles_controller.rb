3e65d817655bd829678a7052eea59a56328e4316
Arun sudharsan <arun.m.sudharsan@gmail.com>
2019-01-17 15:25:23 +0530

    added style to blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,27 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:edit, :destroy , :update , :show]
 
 def new
 	@article = Article.new
 end
 
 
+def create
+	@article = Article.new(article_params)
+	if @article.save
+		flash[:notice] = "article was successfully created"
+	redirect_to article_path(@article)
+else
+	render 'new'
+	end
+end
+
 def edit
-@article = Article.find(params[:id])
+
 end
 
 def show 
-@article = Article.find(params[:id])
+
 end
 
 
@@ -19,27 +30,19 @@ def index
 end
 
 
+
+
 def destroy
-	@article = Article.find(params[:id])
+	
 	@article.destroy
 	flash[:notice] = 'article was successfully destroyed'
 	redirect_to articles_path
 end
 
 
-def create
-	@article = Article.new(article_params)
 
-
-	if @article.save
-		flash[:notice] = "article was successfully created"
-	redirect_to article_path(@article)
-else
-	render 'new'
-	end
-end
 def update
-	 @article = Article.find(params[:id])
+	
 
 	if @article.update(article_params)
 		flash[:notice] = "article was successfully updated"
@@ -50,6 +53,9 @@ def update
 
 end
 
+	def set_article
+		@article = Article.find(params[:id])
+	end
  
 	def article_params
 		params.require(:article).permit(:title, :description)
