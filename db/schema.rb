b90d6ef771ea02a8a5282e9746348ee356cc78cf
Arun sudharsan <arun.m.sudharsan@gmail.com>
2019-01-21 15:22:31 +0530

    added password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_21_085914) do
+ActiveRecord::Schema.define(version: 2019_01_21_094712) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_01_21_085914) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
